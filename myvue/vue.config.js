80a1cf78e5e945d40120be524b4870f0eac6baff
yangxiaobo <17839965308@163.com>
2022-07-05 00:06:27 +0800

    初始化vue

--- myvue/vue.config.js
+++ myvue/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+    alias:{
+      'assets':'@/assets',
+      'common':'@/common',
+      'components':'@/components',
+      'network':'@/network'
+    }
+  }
+
 })
